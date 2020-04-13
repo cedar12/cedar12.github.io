9052ba018eec33adefb8933565d020bcaa5630f7
cedar12 <413338772@qq.com>
2020-04-13 14:34:36 +0800

    index

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
 window.onload=function(){
-	
-	//setTimeout(()=>{
-		//	document.getElementById("cedar-data").classList.remove("down-anim");
-		/*
-			document.getElementById("cedar-data").onmouseover=function(){
-				this.classList.add("hover-anim");
-			}*/
-	//},2000)
 	setTimeout(()=>{
 		document.getElementById("cedar-data").classList.remove("down-anim");
 	},2000)
